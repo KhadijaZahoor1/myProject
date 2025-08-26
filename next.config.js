4c6d03ba1688e0f53c5a501dbdc2f2d9efc26a90
Umer Mushtaq <umer.techsupportone@nxvt.com>
2025-08-26 18:37:21 +0500

    added export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ✅ enables static export for App Router
   images: {
     domains: ["i.dummyjson.com"],
   },
+  basePath: '/myProject',     // 👈 replace with your repo name
+  assetPrefix: '/myProject/',
 };
 
 module.exports = nextConfig;
